4c2c783e10ed41d63eea6b3205342c3b103a29af
Aliyah <a.eghaghe@outlook.com>
2023-10-02 14:24:56 +0200

    .....

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,8 +8,8 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("test.controller.View1", {
-            resolveUrl: function(sUrl) {
-                return sap.ui.require.toUrl("card_v3/assets/avatars/" + sUrl);
+            onInit: function () {
+
             }
         });
     });
