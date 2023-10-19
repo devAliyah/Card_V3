1f0e0c7e739552fef72ca93718f945b095abad96
Aliyah <a.eghaghe@outlook.com>
2023-10-19 12:01:33 +0200

    OnPress
    Stap 1: new page

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/m/MessageToast',
+    'sap/ui/Device',
+    'sap/ui/model/json/JSONModel',
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("test.controller.View1", {
-            onInit: function () {
+        
 
-            }
+
+            
+            mcPress: function(evt) {
+                this.getRouter().navTo("mc");
+            },
+            burgerPress: function(evt) {
+                MessageToast.show("The image has been pressed");
+            },
+
+                zusjePress: function(evt) {
+                    MessageToast.show("The image has been pressed");
+                },
+
+                getRouter: function() {
+                    return this.getOwnerComponent().getRouter();
+                }
+        
         });
     });
+
+    
